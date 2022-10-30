843871a4f88b468e7bcef62c6e6ed9b27ca1b7c4
Marta Bettoni <bettonimarta99@gmail.com>
2022-10-30 11:17:43 +0100

    create the loop but tie make the computer win

--- script.js
+++ script.js
@@ -8,16 +8,27 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         return `It's a tie! You both picked ${playerSelection}`;
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore++; 
         return "You win! Rock beats Scissors";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore++;
         return "You win! Paper beats Rock";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore++;
         return "You win! Scissors beats Paper";
     } else {
+        computerScore++;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
+for (let i=0; i<5; i++) {
 const computerSelection = getComputerChoice();
 const playerSelection = prompt("Choose between Rock, Paper or Scissors").toLowerCase();
 console.log(playRound(playerSelection, computerSelection));
+console.log("Your score = " + playerScore)
+console.log("Computer's score = " + computerScore)
+}
