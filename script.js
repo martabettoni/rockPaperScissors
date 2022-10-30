7ff8781955ba406bb081b2cbe4a46938207a7093
Marta Bettoni <bettonimarta99@gmail.com>
2022-10-30 10:18:10 +0100

    create first and second function

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+function getComputerChoice() {
+    let randomPick=["Rock", "Paper", "Scissors"];
+    return randomPick[Math.floor(Math.random() * randomPick.length)];
+}
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a tie! You both picked ${playerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats Paper";
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
